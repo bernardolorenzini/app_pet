69aed683e394b24865d1e9acefed6a41ba89532b
Bernardo Lorenzini <lorenzinibernardo@gmail.com>
2024-01-24 21:55:14 -0300

    Initial Commit

---
+++ app_pet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app_pet
+//
+//  Created by Bernardo Lorenzini on 24/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app_pet/app_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_petApp.swift
+//  app_pet
+//
+//  Created by Bernardo Lorenzini on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct app_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
